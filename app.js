79c5f1fdc7e39d05e685b843376d781cb3f05a44
xRyumax <xmaurize16x@gmail.com>
2025-04-08 10:23:54 -0500

    hola

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-console.log("hola como mundo")
-console.log("el segundo objeto de prueba")
-console.log("el tercer objeto de prueba")
-console.log("el cuarto objeto de prueba")
-console.log("el quinto objeto de prueba")
+
+const showMessage = (message) => {
+    return message;
+};
+
+console.log(showMessage('Hola como estas'));
